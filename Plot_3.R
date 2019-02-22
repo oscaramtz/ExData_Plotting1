544539922e7f6f4a776214c31d0fb3d61662ba0c
Oscar Alfredo Martinez Martinez <oscaramtz@users.noreply.github.com>
2019-02-22 09:42:04 -0600

    Update and rename Plot_3 to Plot_3.R

--- Plot_3.R
+++ Plot_3.R
@@ -1,4 +1,18 @@
+## Getting data form ./data directory
+filepath <- "./data/household_power_consumption.txt"
 
+## Loading process 
+install.packages("sqldf")
+library(sqldf)
+library(lubridate)
+
+### Subsetting data
+keptvar <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+
+### 
+hPowerCons_subset <- read.csv.sql(filepath, keptvar, sep=";")
+hPowerCons_subset$Date <- dmy(hPowerCons_subset$Date)
+hPowerCons_subset$DateTime <- as.POSIXct(paste(hPowerCons_subset$Date, hPowerCons_subset$Time), format="%Y-%m-%d %H:%M:%S")
 
 
 ## plot 3
